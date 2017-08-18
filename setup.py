b8783061f76452e9263b04829505eae028bbf2a6
jneilliii <jneilliii+github@gmail.com>
2017-08-18 13:37:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,13 +21,13 @@ plugin_version = "0.1.5"
 plugin_description = """Displays detailed progress on the LCD screen"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Dattas Moonchaser"
+plugin_author = "jneilliii"
 
 # The plugin's author's mail address.
-plugin_author_email = "dattasmoon@gmail.com"
+plugin_author_email = "jneilliii@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/dattas/OctoPrint-DetailedProgress"
+plugin_url = "https://github.com/jneilliii/OctoPrint-DetailedProgress"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
